b3c2d3bf6a904b970341930f00fc902ebb097d7b
Dave English <dave.english@digital.hmrc.gov.uk>
2017-06-08 11:38:51 +0100

    Fix after rebase

--- app/uk/gov/hmrc/gform/services/SubmissionService.scala
+++ app/uk/gov/hmrc/gform/services/SubmissionService.scala
@@ -44,7 +44,8 @@ object SubmissionService {
         fieldValue.`type` match {
           case Address(_) => Address.fields(fieldValue.id)
           case Date(_, _, _) => Date.fields(fieldValue.id)
-          case Text(_, _) | Choice(_, _, _, _, _) | Group(_) => List(fieldValue.id) // TODO - added Group just to compile; remove if possible
+          case FileUpload() => List(fieldValue.id)
+          case Text(_, _) | Choice(_, _, _, _, _) | Group(_, _) => List(fieldValue.id) // TODO - added Group just to compile; remove if possible
         }
 
       val formFieldAndFieldValues: List[Opt[(FormField, FieldValue)]] =
