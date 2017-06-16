8fe301be4d35063fc6bdf8ae559f0411cf4398a1
Dave English <dave.english@digital.hmrc.gov.uk>
2017-06-16 09:30:14 +0100

    Revert version changes because app not starting in MDTP

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -11,44 +11,29 @@ object MicroServiceBuild extends Build with MicroService {
 
   override lazy val appDependencies: Seq[ModuleID] = compile ++ test()
 
-  val parsebackVersion = "0.3"
-  val microserviceBootstrapVersion = "5.15.0"
-  val domainVersion = "4.1.0"
-  val hmrcTestVersion = "2.3.0"
-  val playConfigVersion = "4.3.0"
-  val playAuthorisationVersion = "4.3.0"
-  val playHealthVersion = "2.1.0"
-  val playReactivemongoVersion = "5.2.0"
-  val playUrlBindersVersion = "2.1.0"
-  val playJsonLoggerVersion = "3.1.0"
-
-  val catsVersion = "0.9.0"
-  val playAuditing = "2.9.0"
-  val pdfboxVersion = "2.0.6"
-  val spdfVersion = "1.4.0"
+  val ParsebackVersion = "0.3"
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "play-reactivemongo" % playReactivemongoVersion,
-    "uk.gov.hmrc" %% "microservice-bootstrap" % microserviceBootstrapVersion,
-    "uk.gov.hmrc" %% "play-authorisation" % playAuthorisationVersion,
-    "uk.gov.hmrc" %% "play-health" % playHealthVersion,
-    "uk.gov.hmrc" %% "play-url-binders" % playUrlBindersVersion,
-    "uk.gov.hmrc" %% "play-config" % playConfigVersion,
-    "uk.gov.hmrc" %% "play-auditing" % playAuditing,
-    "uk.gov.hmrc" %% "logback-json-logger" % playJsonLoggerVersion,
-    "uk.gov.hmrc" %% "domain" % domainVersion,
-    "com.codecommit" %% "parseback-core" % parsebackVersion,
-    "com.codecommit" %% "parseback-cats" % parsebackVersion,
+    "uk.gov.hmrc" %% "play-reactivemongo" % "5.1.0",
+    "uk.gov.hmrc" %% "microservice-bootstrap" % "5.8.0",
+    "uk.gov.hmrc" %% "play-authorisation" % "4.2.0",
+    "uk.gov.hmrc" %% "play-health" % "2.0.0",
+    "uk.gov.hmrc" %% "play-url-binders" % "2.0.0",
+    "uk.gov.hmrc" %% "play-config" % "3.1.0",
+    "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
+    "uk.gov.hmrc" %% "domain" % "4.0.0",
+    "com.codecommit" %% "parseback-core" % ParsebackVersion,
+    "com.codecommit" %% "parseback-cats" % ParsebackVersion,
     "org.julienrf" %% "play-json-derived-codecs" % "3.3",
-    "org.typelevel" %% "cats" % catsVersion,
-    "org.apache.pdfbox" % "pdfbox" % pdfboxVersion,
-    "io.github.cloudify" %% "spdf" % spdfVersion
+    "org.typelevel" %% "cats" % "0.9.0",
+    "org.apache.pdfbox" % "pdfbox" % "2.0.4",
+    "io.github.cloudify" %% "spdf" % "1.4.0"
   )
 
   def test(scope: String = "test,it") = Seq(
-    "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
-    "org.scalatest" %% "scalatest" % "3.0.3" % scope,
+    "uk.gov.hmrc" %% "hmrctest" % "2.2.0" % scope,
+    "org.scalatest" %% "scalatest" % "3.0.1" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
     "org.scalamock" %% "scalamock-scalatest-support" % "3.4.2" % scope,
