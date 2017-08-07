e6b71eae627c633b64f4cc9e099f14cc6b932084
Pawel Panasewicz <pawel.panasewicz@digital.hmrc.gov.uk>
2017-08-07 15:22:30 +0100

    rename package

--- app/uk/gov/hmrc/gform/fileupload/FileUploadFrontendConnector.scala
+++ app/uk/gov/hmrc/gform/fileupload/FileUploadFrontendConnector.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.gform.fileUpload
+package uk.gov.hmrc.gform.fileupload
 
 import akka.util.ByteString
 import uk.gov.hmrc.gform.sharedmodel.form.{ EnvelopeId, FileId }
--- test/uk/gov/hmrc/gform/sharedmodel/ExampleData.scala
+++ test/uk/gov/hmrc/gform/sharedmodel/ExampleData.scala
@@ -18,7 +18,7 @@ package uk.gov.hmrc.gform.sharedmodel
 
 import java.time.LocalDateTime
 
-import uk.gov.hmrc.gform.fileUpload.RouteEnvelopeRequest
+import uk.gov.hmrc.gform.fileupload.RouteEnvelopeRequest
 import uk.gov.hmrc.gform.sharedmodel.form._
 import uk.gov.hmrc.gform.sharedmodel.formtemplate.{AnyText, _}
 import uk.gov.hmrc.gform.submission.{DmsMetaData, Submission, SubmissionRef}
