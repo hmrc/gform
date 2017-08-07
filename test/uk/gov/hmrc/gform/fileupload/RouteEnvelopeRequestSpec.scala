f45ce00957082b46e7c0131d03f40ee398b5b249
Pawel Panasewicz <pawel.panasewicz@digital.hmrc.gov.uk>
2017-08-07 15:37:36 +0100

    formatting

--- test/uk/gov/hmrc/gform/fileupload/RouteEnvelopeRequestSpec.scala
+++ test/uk/gov/hmrc/gform/fileupload/RouteEnvelopeRequestSpec.scala
@@ -1,6 +1,22 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.gform.fileupload
 
-import play.api.libs.json.{JsObject, JsResult, JsSuccess, Json}
+import play.api.libs.json.{ JsObject, JsResult, JsSuccess, Json }
 import uk.gov.hmrc.gform.Spec
 import uk.gov.hmrc.gform.sharedmodel.ExampleData
 import uk.gov.hmrc.gform.sharedmodel.form.Form
@@ -18,10 +34,9 @@ class RouteEnvelopeRequestSpec extends Spec {
     )
 
     val writtenJsObject: JsObject = RouteEnvelopeRequest.format.writes(routeEnvelopeRequest)
-    println(Json.prettyPrint (writtenJsObject))
+    println(Json.prettyPrint(writtenJsObject))
     jsObject shouldBe writtenJsObject
     RouteEnvelopeRequest.format.reads(jsObject) should be(JsSuccess(routeEnvelopeRequest))
   }
-
 
 }
