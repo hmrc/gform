bfa770e7bd0609e2fe4b19163753235b503cecc1
Pawel Panasewicz <pawel.panasewicz@digital.hmrc.gov.uk>
2017-08-08 12:20:38 +0100

    application.conf now configures content types which can be consumed by file upload. This setting is exposed via exposed config

--- app/uk/gov/hmrc/gform/config/config.scala
+++ app/uk/gov/hmrc/gform/config/config.scala
@@ -18,13 +18,19 @@ package uk.gov.hmrc.gform.config
 
 import com.typesafe.config.{ Config => TypeSafeConfig }
 import pureconfig._
+import uk.gov.hmrc.gform.sharedmodel.config.ContentType
 
 case class AppConfig(
-  appName: String,
-  formExpiryDays: Int,
-  formMaxAttachmentSizeMB: Int,
-  formMaxAttachments: Int
-)
+    appName: String,
+    formExpiryDays: Int,
+    formMaxAttachmentSizeMB: Int,
+    formMaxAttachments: Int,
+    /*we can't override list in app-config-base:*/
+    contentTypesSeparatedByPipe: String
+) {
+
+  def contentTypes: List[ContentType] = contentTypesSeparatedByPipe.split('|').toList.map(ContentType.apply)
+}
 
 object AppConfig {
 
@@ -35,6 +41,7 @@ object AppConfig {
     appConfig.formExpiryDays.verifyThat(_ > 0, s"'formExpiryDays' must be positive, was ${appConfig.formExpiryDays}")
     appConfig.formMaxAttachments.verifyThat(_ > 0, s"'formMaxAttachments' must be positive, was ${appConfig.formMaxAttachments}")
     appConfig.formMaxAttachmentSizeMB.verifyThat(_ > 0, s"'formMaxAttachmentSizeMB' must be positive, was ${appConfig.formMaxAttachmentSizeMB}")
+    appConfig.contentTypes.length.verifyThat(_ > 0, s"'contentTypesSeparatedByPipe' is not set")
 
     appConfig
   }
---
+++ app/uk/gov/hmrc/gform/fileupload/Helper.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.gform.fileupload
+
+import java.time.format.DateTimeFormatter
+
+import play.api.http.HeaderNames.LOCATION
+import play.api.libs.json.{ JsObject, Json }
+import uk.gov.hmrc.gform.sharedmodel.form.EnvelopeId
+import uk.gov.hmrc.gform.sharedmodel.formtemplate.FormTemplateId
+import uk.gov.hmrc.gform.time.TimeProvider
+import uk.gov.hmrc.play.http.HttpResponse
+
+class Helper(config: FUConfig, timeProvider: TimeProvider) {
+
+  def createEnvelopeRequestBody(formTemplateId: FormTemplateId): JsObject = Json.obj(
+    "constraints" -> Json.obj(
+      "contentTypes" -> contentTypesJson,
+      "maxItems" -> config.maxItems,
+      "masSize" -> config.maxSize,
+      "maxSizePerItem" -> config.maxSizePerItem
+    ),
+    "callbackUrl" -> "someCallback",
+    "expiryDate" -> s"$envelopeExpiryDate",
+    "metadata" -> Json.obj(
+      "application" -> "gform",
+      "formTemplateId" -> s"${formTemplateId.value}"
+    )
+  )
+
+  /**
+   * There must be Location header. If not this is exceptional situation!
+   */
+  def extractEnvelopId(resp: HttpResponse): EnvelopeId = resp.header(LOCATION) match {
+    case Some(EnvelopeIdExtractor(envelopeId)) => EnvelopeId(envelopeId)
+    case Some(location) => throw new SpoiltLocationHeader(location)
+    case _ => throw new SpoiltLocationHeader(s"Header $LOCATION not found")
+  }
+
+  private lazy val EnvelopeIdExtractor = "envelopes/([\\w\\d-]+)$".r.unanchored
+  private def envelopeExpiryDate = timeProvider.localDateTime().plusDays(config.expiryDays.toLong).format(formatter)
+  private val formatter = DateTimeFormatter.ofPattern("YYYY-MM-dd'T'HH:mm:ss'Z'")
+  private lazy val contentTypesJson = Json.toJson(config.contentTypes)
+}
