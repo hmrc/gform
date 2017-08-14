4fa8397f263f5ed066d6830160a58998e4a493a1
Dave English <dave.english@digital.hmrc.gov.uk>
2017-08-14 17:31:12 +0100

    More extensive fix of file upload contentb type so that all works and tests pass

--- it/uk/gov/hmrc/gform/ExposedConfigSpec.scala
+++ it/uk/gov/hmrc/gform/ExposedConfigSpec.scala
@@ -14,9 +14,9 @@ class ExposedConfigSpec extends support.ITSpec {
       formExpiryDays = 30,
       List(
         ContentType.`application/pdf`,
-        ContentType.`application/xml; charset=UTF-8`,
+        ContentType.`application/xml`,
         ContentType.`image/jpeg`,
-        ContentType.`text/plain`,
+        ContentType.`text/xml`,
         ContentType.`application/vnd.ms-excel`,
         ContentType.`application/vnd.openxmlformats-officedocument.spreadsheetml.sheet`
       )
