1a7adb12fd4b8036e33ad9d02ec6a806afbb8c3f
pawel.panasewicz <pawel.panasewicz@gmail.com>
2017-08-22 13:59:19 +0100

    utility for uploading templates

--- it/uk/gov/hmrc/gform/TemplatesInSampleDataSpec.scala
+++ it/uk/gov/hmrc/gform/TemplatesInSampleDataSpec.scala
@@ -4,23 +4,45 @@ import java.nio.file.{Files, Paths}
 
 import collection.JavaConverters._
 import play.api.libs.json.{JsValue, Json}
+import uk.gov.hmrc.gform.gformbackend.GformConnector
+import uk.gov.hmrc.gform.wshttp.{TestWSHttp, WSHttp}
+import uk.gov.hmrc.play.http.HeaderCarrier
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 
 class TemplatesInSampleDataSpec extends support.ITSpec {
 
    "Test that templates in sample data can be uploaded" in eventually {
-       templates foreach { templateFile =>
+     TemplatesInSampleDataSpec.templates foreach { templateFile =>
         val template: JsValue = Json.parse(Files.newInputStream(templateFile))
         gformConnector.upsertTemplate(template).futureValue shouldBe (()) withClue templateFile.toString
       }
     }
+}
 
+object TemplatesInSampleDataSpec {
   lazy val templates = Files
     .newDirectoryStream(Paths.get("sample-data"))
     .asScala
     .filter(Files.isRegularFile(_))
-    .filter(_.getFileName.toString.matches("template-add(.*)json"))
+    .filter(_.getFileName.toString.matches("template-aaa(.*)json"))
     .toList
+}
 
+object UploadAllTemplates extends App {
+
+  val wsclient = TestWSHttp
+  val baseUrl = """http://localhost:9196/gform"""
+  val gformConnector = new GformConnector(wsclient, baseUrl)
+  implicit val hc = HeaderCarrier()
+
+  TemplatesInSampleDataSpec.templates foreach { templateFile =>
+    println(s"uploading $templateFile")
+    val template: JsValue = Json.parse(Files.newInputStream(templateFile))
+    gformConnector.upsertTemplate(template).onComplete{
+      case Failure(f) => println(f)
+      case Success(_) => ()
+    }
+  }
 }
