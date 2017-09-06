be6e212389804aeb147c64ae79442407655b56b3
Tenoch De La Cruz <tenoch.delacruz@digital.hmrc.gov.uk>
2017-09-06 14:00:45 +0100

    EEITT-1230: Merged latest master

--- test/uk/gov/hmrc/gform/core/parsers/ValueParserSpec.scala
+++ test/uk/gov/hmrc/gform/core/parsers/ValueParserSpec.scala
@@ -192,7 +192,7 @@ class ValueParserSpec extends Spec {
     "description",
     Some(Default),
     DmsSubmission("nino", "BT-NRU-Environmental", "FinanceOpsCorpT"),
-    AuthConfig(AuthConfigModule("TEST"), RegimeId("TEST"), None, None),
+    HMRCAuthConfig(AuthConfigModule("TEST"), Some(RegimeId("TEST")), None, None),
     "submitSuccessUrl",
     "submitErrorUrl",
     List.empty[Section],
