159ab89a65ad55c7e59d57c912656d8a9a42a954
daniel.connelly <danile.connelly@digital.hmrc.gov.uk>
2017-09-07 18:39:38 +0100

    changes made

--- app/uk/gov/hmrc/gform/validation/ValidationService.scala
+++ app/uk/gov/hmrc/gform/validation/ValidationService.scala
@@ -1,16 +1,30 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.gform.validation
 
 import uk.gov.hmrc.gform.bank_account_reputation.BankAccountReputationConnector
-import uk.gov.hmrc.gform.des.{AddressDes, DesConnector}
-import uk.gov.hmrc.play.http.{HeaderCarrier, NotFoundException}
+import uk.gov.hmrc.gform.des.{ AddressDes, DesConnector }
+import uk.gov.hmrc.play.http.{ HeaderCarrier, NotFoundException }
 
-import scala.concurrent.Future
-
+import scala.concurrent.{ ExecutionContext, Future }
 
 class ValidationService(desConnector: DesConnector, bankAccountReputationConnector: BankAccountReputationConnector) {
 
-
-  def callDes(utr: String, postCode: String)(implicit hc: HeaderCarrier): Future[Boolean] = {
+  def callDes(utr: String, postCode: String)(implicit hc: HeaderCarrier, ex: ExecutionContext): Future[Boolean] = {
     def compare(address: AddressDes) = {
       address.postalCode.replace(" ", "").equalsIgnoreCase(postCode.replace(" ", "")) || address.postalCode == "Valid"
     }
@@ -20,8 +34,7 @@ class ValidationService(desConnector: DesConnector, bankAccountReputationConnect
       }
   }
 
-  def callBRS(accountNumber: String, sortCode: String): Future[Boolean] =
+  def callBRS(accountNumber: String, sortCode: String)(implicit hc: HeaderCarrier, ex: ExecutionContext): Future[Boolean] =
     bankAccountReputationConnector.exists(accountNumber, sortCode).map(_.accountNumberWithSortCodeIsValid)
-
 
 }
