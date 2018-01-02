5b1d067b99704c53a33285be9f0608571da32cb5
Dave English <dave.english@digital.hmrc.gov.uk>
2018-01-02 13:34:36 +0000

    2018 copyright

--- test/uk/gov/hmrc/gform/core/TemplateValidatorSpec.scala
+++ test/uk/gov/hmrc/gform/core/TemplateValidatorSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -466,4 +466,3 @@ class TemplateValidatorSpec extends Spec {
   private lazy val jsonSchema = FormTemplateSchema.jsonSchema
 
 }
-
--- test/uk/gov/hmrc/gform/sharedmodel/ExprSpec.scala
+++ test/uk/gov/hmrc/gform/sharedmodel/ExprSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -67,4 +67,3 @@ class ExprSpec extends Spec {
   }
 
 }
-
