7c0cc2243da79287db7d20cfc7c43cdca76c4554
Dave English <dave.english@digital.hmrc.gov.uk>
2018-01-02 13:45:57 +0000

    2018 copyright

--- it/uk/gov/hmrc/gform/TemplatesInSampleDataSpec.scala
+++ it/uk/gov/hmrc/gform/TemplatesInSampleDataSpec.scala
@@ -1,25 +1,41 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.gform
 
-import java.nio.file.{Files, Paths}
+import java.nio.file.{ Files, Paths }
 
 import collection.JavaConverters._
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.{ JsValue, Json }
 import uk.gov.hmrc.gform.gformbackend.GformConnector
-import uk.gov.hmrc.gform.wshttp.{TestWSHttpIT, WSHttp}
+import uk.gov.hmrc.gform.wshttp.{ TestWSHttpIT, WSHttp }
 import uk.gov.hmrc.http.HeaderCarrier
 
 import uk.gov.hmrc.play.http.logging.MdcLoggingExecutionContext._
-import scala.util.{Failure, Success}
+import scala.util.{ Failure, Success }
 
 class TemplatesInSampleDataSpec extends support.ITSpec {
 
-//   "Test that templates in sample data can be uploaded" in eventually {
-//     TemplatesInSampleDataSpec.templates foreach { templateFile =>
-//        val template: JsValue = Json.parse(Files.newInputStream(templateFile))
-//       println(template)
-//        gformConnector.upsertTemplate(template).futureValue shouldBe (()) withClue templateFile.toString
-//      }
-//    }
+  //   "Test that templates in sample data can be uploaded" in eventually {
+  //     TemplatesInSampleDataSpec.templates foreach { templateFile =>
+  //        val template: JsValue = Json.parse(Files.newInputStream(templateFile))
+  //       println(template)
+  //        gformConnector.upsertTemplate(template).futureValue shouldBe (()) withClue templateFile.toString
+  //      }
+  //    }
 }
 
 object TemplatesInSampleDataSpec {
@@ -41,7 +57,7 @@ object UploadAllTemplates extends App {
   TemplatesInSampleDataSpec.templates foreach { templateFile =>
     println(s"uploading $templateFile")
     val template: JsValue = Json.parse(Files.newInputStream(templateFile))
-    gformConnector.upsertTemplate(template).onComplete{
+    gformConnector.upsertTemplate(template).onComplete {
       case Failure(f) => println(f)
       case Success(_) => ()
     }
--- it/uk/gov/hmrc/gform/support/ITSpec.scala
+++ it/uk/gov/hmrc/gform/support/ITSpec.scala
@@ -1,9 +1,25 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.gform.support
 
-import org.scalatestplus.play.{BaseOneServerPerTest, FakeApplicationFactory}
+import org.scalatestplus.play.{ BaseOneServerPerTest, FakeApplicationFactory }
 import play.api.ApplicationLoader.Context
 import play.api.libs.ws.WSClient
-import play.api.{Application, Configuration, Environment}
+import play.api.{ Application, Configuration, Environment }
 import play.core.DefaultWebCommands
 import uk.gov.hmrc.gform.ApplicationModule
 import uk.gov.hmrc.gform.gformbackend.GformConnector
@@ -11,9 +27,9 @@ import uk.gov.hmrc.gform.wshttp.WSHttp
 import uk.gov.hmrc.http.HeaderCarrier
 
 /**
-  * This spec provides running play application for every test.
-  */
-trait ITSpec extends ITSpecBase with BaseOneServerPerTest with /*TODO MongoSpecSupport with */FakeApplicationFactory {
+ * This spec provides running play application for every test.
+ */
+trait ITSpec extends ITSpecBase with BaseOneServerPerTest with /*TODO MongoSpecSupport with */ FakeApplicationFactory {
 
   override def fakeApplication(): Application = application
 
@@ -22,14 +38,12 @@ trait ITSpec extends ITSpecBase with BaseOneServerPerTest with /*TODO MongoSpecS
   private lazy val mongoDbName: String = "test-" + this.getClass.getSimpleName
   private lazy val env: Environment = Environment.simple()
   private lazy val configurationOverridings = Map(
-    "mongodb.uri" -> s"mongodb://localhost:27017/$mongoDbName"
-  )
+    "mongodb.uri" -> s"mongodb://localhost:27017/$mongoDbName")
   private lazy val context: Context = Context(
     environment = env,
     sourceMapper = None,
     webCommands = new DefaultWebCommands(),
-    initialConfiguration = Configuration.load(env)
-  )
+    initialConfiguration = Configuration.load(env))
   private lazy val applicationModule = new ApplicationModule(context) {
     override lazy val httpFilters = Nil
   }
