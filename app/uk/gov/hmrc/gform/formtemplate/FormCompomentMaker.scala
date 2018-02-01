eec4d34e241a7a4149030907fc96123d8ecfb8d7
cavster <colm.m.cavanagh@gmail.com>
2018-02-01 14:13:29 +0000

    [CC]submitinfoonly mode will trow an error if there is no value or if it's invalid

--- app/uk/gov/hmrc/gform/formtemplate/FormCompomentMaker.scala
+++ app/uk/gov/hmrc/gform/formtemplate/FormCompomentMaker.scala
@@ -90,19 +90,19 @@ class FormCompomentMaker(json: JsValue) {
     presentationHint = presHint,
     errorMessage = errorMessage)
 
-  private lazy val optMES: Opt[MES] = (submitMode, mandatory) match {
+  private lazy val optMES: Opt[MES] = (submitMode, mandatory, optMaybeValueExpr) match {
     //format: OFF
-    case IsThisAnInfoField()                                          => MES(mandatory = true,  editable = false, submissible = false, derived = false).asRight
-    case (Some(IsStandard()) | None, Some(IsTrueish()) | None)        => MES(mandatory = true,  editable = true,  submissible = true, derived = false).asRight
-    case (Some(IsReadOnly()),        Some(IsTrueish()) | None)        => MES(mandatory = true,  editable = false, submissible = true, derived = false).asRight
-    case (Some(IsInfo()),            Some(IsTrueish()) | None)        => MES(mandatory = true,  editable = false, submissible = false, derived = false).asRight
-    case (Some(IsStandard()) | None, Some(IsFalseish()))              => MES(mandatory = false, editable = true,  submissible = true, derived = false).asRight
-    case (Some(IsInfo()),            Some(IsFalseish()))              => MES(mandatory = false, editable = false, submissible = false, derived = false).asRight
-    case (Some(IsDerived()),         Some(IsTrueish()) | None)        => MES(mandatory = true, editable = false, submissible = false, derived = true).asRight
-    case (Some(IsDerived()),         Some(IsFalseish()))              => MES(mandatory = false, editable = false, submissible = false, derived = true).asRight
-    case (Some(IsNonSubmissible()),  Some(IsFalseish()) | None)       => MES(mandatory = false, editable = true, submissible = false, derived = false).asRight
-    case (Some(IsNonSubmissible()),  Some(IsTrueish())  | None)       => MES(mandatory = true, editable = true, submissible = false, derived = false).asRight
-    case (Some(IsSummaryInfoOnly()), Some(IsTrueish()) | Some(IsFalseish()) | None) =>
+    case IsThisAnInfoField()                                            => MES(mandatory = true,  editable = false, submissible = false, derived = false).asRight
+    case (Some(IsStandard()) | None, Some(IsTrueish()) | None, _)        => MES(mandatory = true,  editable = true,  submissible = true, derived = false).asRight
+    case (Some(IsReadOnly()),        Some(IsTrueish()) | None, _)       => MES(mandatory = true,  editable = false, submissible = true, derived = false).asRight
+    case (Some(IsInfo()),            Some(IsTrueish()) | None, _)       => MES(mandatory = true,  editable = false, submissible = false, derived = false).asRight
+    case (Some(IsStandard()) | None, Some(IsFalseish()), _)              => MES(mandatory = false, editable = true,  submissible = true, derived = false).asRight
+    case (Some(IsInfo()),            Some(IsFalseish()), _)              => MES(mandatory = false, editable = false, submissible = false, derived = false).asRight
+    case (Some(IsDerived()),         Some(IsTrueish()) | None, _)        => MES(mandatory = true, editable = false, submissible = false, derived = true).asRight
+    case (Some(IsDerived()),         Some(IsFalseish()), _)              => MES(mandatory = false, editable = false, submissible = false, derived = true).asRight
+    case (Some(IsNonSubmissible()),  Some(IsFalseish()) | None, _)       => MES(mandatory = false, editable = true, submissible = false, derived = false).asRight
+    case (Some(IsNonSubmissible()),  Some(IsTrueish())  | None, _)       => MES(mandatory = true, editable = true, submissible = false, derived = false).asRight
+    case (Some(IsSummaryInfoOnly()), Some(IsTrueish()) | Some(IsFalseish()) | None, Right(Some(_))) =>
       MES(mandatory = true, editable = false, submissible = false, derived = false, onlyShowOnSummary = true).asRight
     case otherwise                                              => UnexpectedState(s"Expected 'standard', summaryinfoonly,'notsubmitted' ,readonly' or 'info' string or nothing for submitMode and expected 'true' or 'false' string or nothing for mandatory field value, got: $otherwise").asLeft
     //format: ON
@@ -447,7 +447,7 @@ class FormCompomentMaker(json: JsValue) {
   }
 
   private final object IsThisAnInfoField {
-    def unapply(ignoredArgs: (Option[String], Option[String])) = `type`.getOrElse(None).isInstanceOf[InfoRaw.type]
+    def unapply(ignoredArgs: (Option[String], Option[String], _)) = `type`.getOrElse(None).isInstanceOf[InfoRaw.type]
   }
 
   private def parse[T: Reads, R](path: String, validate: T => Opt[R]): Opt[Option[R]] = {
