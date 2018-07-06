feed2306c5d92895fefdaa43ac25075785304927
Dave English <dave.english@digital.hmrc.gov.uk>
2018-07-06 11:37:03 +0100

    Non-hmrc minor version bumps

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -15,7 +15,7 @@ object MicroServiceBuild extends Build with MicroService {
   val compile = Seq(
     ws,
     "uk.gov.hmrc" %% "play-reactivemongo" % "5.2.0",
-    "com.github.pureconfig" %% "pureconfig" % "0.8.0",
+    "com.github.pureconfig" %% "pureconfig" % "0.9.1",
     "uk.gov.hmrc" %% "http-caching-client" % "7.1.0",
     "uk.gov.hmrc" %% "microservice-bootstrap" % "6.18.0",
     "uk.gov.hmrc" %% "play-url-binders" % "2.1.0",
@@ -24,24 +24,23 @@ object MicroServiceBuild extends Build with MicroService {
     "com.codecommit" %% "parseback-cats" % parsebackVersion,
     "org.julienrf" %% "play-json-derived-codecs" % "3.3",
     "org.typelevel" %% "cats" % "0.9.0",
-    "org.apache.pdfbox" % "pdfbox" % "2.0.7",
+    "org.apache.pdfbox" % "pdfbox" % "2.0.11",
     "io.github.cloudify" %% "spdf" % "1.4.0",
-    "com.chuusai" %% "shapeless" % "2.3.2",
-    "com.github.pureconfig" %% "pureconfig" % "0.7.2",
-    "com.github.mpilquist" %% "simulacrum" % "0.11.0",
+    "com.chuusai" %% "shapeless" % "2.3.3",
+    "com.github.mpilquist" %% "simulacrum" % "0.12.0",
     "uk.gov.hmrc" %% "auth-client" % "2.6.0" // we need AffinityGroup type to stay in sync with frontend
   )
 
 
   def test(scope: String = "test,it") = Seq(
     "uk.gov.hmrc" %% "hmrctest" % "3.0.0" % scope,
-    "org.scalatest" %% "scalatest" % "3.0.4" % scope,
+    "org.scalatest" %% "scalatest" % "3.0.5" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
     "org.scalamock" %% "scalamock-scalatest-support" % "3.6.0" % scope,
     "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % scope,
     "uk.gov.hmrc" %% "reactivemongo-test" % "2.0.0" % scope,
-    "org.jsoup" % "jsoup" % "1.10.3" % scope
+    "org.jsoup" % "jsoup" % "1.11.3" % scope
   )
 
 }
