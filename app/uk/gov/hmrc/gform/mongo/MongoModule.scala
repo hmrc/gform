512b00a0d68c4a2aa6f2a35da777c367e15a2c39
Dave English <dave.english@digital.hmrc.gov.uk>
2018-07-13 11:08:08 +0100

    Remove configInApp no longer needed for ReactiveMongo

--- app/uk/gov/hmrc/gform/mongo/MongoModule.scala
+++ app/uk/gov/hmrc/gform/mongo/MongoModule.scala
@@ -16,40 +16,15 @@
 
 package uk.gov.hmrc.gform.mongo
 
-import java.io.File
-
-import akka.actor.ActorSystem
-import akka.stream.Materializer
-import play.api.Mode.Mode
-import play.api.http.{ HttpErrorHandler, HttpRequestHandler }
-import play.api.{ Application, Configuration }
 import play.modules.reactivemongo.ReactiveMongoComponentImpl
 import reactivemongo.api.DefaultDB
-import uk.gov.hmrc.gform.ApplicationModule
-import uk.gov.hmrc.gform.config.ConfigModule
 import uk.gov.hmrc.gform.playcomponents.PlayComponents
-
-import scala.concurrent.Future
 
 class MongoModule(playComponents: PlayComponents) {
 
-  //WARN: this is because some genius passed application to the ReactiveMongoComponentImpl instead of only config
-  //If you take a closer look you will see that ReactiveMongoComponentImpl uses only `configuration`
-  private val configInApp = new Application {
-    override def configuration: Configuration = playComponents.context.initialConfiguration
-    override def path: File = ???
-    override def classloader: ClassLoader = ???
-    override def mode: Mode = ???
-    override def actorSystem: ActorSystem = ???
-    override implicit def materializer: Materializer = ???
-    override def requestHandler: HttpRequestHandler = ???
-    override def errorHandler: HttpErrorHandler = ???
-    override def stop(): Future[_] = ???
-  }
-
   val reactiveMongoComponent: ReactiveMongoComponentImpl =
     new ReactiveMongoComponentImpl(
-      configInApp.configuration,
+      playComponents.context.initialConfiguration,
       playComponents.context.environment,
       playComponents.context.lifecycle)
   val mongo: () => DefaultDB = reactiveMongoComponent.mongoConnector.db
