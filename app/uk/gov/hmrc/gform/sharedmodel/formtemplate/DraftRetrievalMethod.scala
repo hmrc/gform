668c1d328d9e92afe3deeef06929e649660be871
Dave English <dave.english@digital.hmrc.gov.uk>
2018-10-08 09:01:54 +0100

    Improve error message parsing DraftRetrievalMethod

--- app/uk/gov/hmrc/gform/sharedmodel/formtemplate/DraftRetrievalMethod.scala
+++ app/uk/gov/hmrc/gform/sharedmodel/formtemplate/DraftRetrievalMethod.scala
@@ -37,7 +37,7 @@ object DraftRetrievalMethod {
         case JsString("formAccessCodeForAgents") =>
           JsSuccess(FormAccessCodeForAgents)
         case JsString(err) =>
-          JsError(s"only two valid draft retrieval methods: onePerUser, or formAccessCodeForAgents; $err is not valid")
+          JsError(s"only two values are allowed for draftRetrievalMethod: either onePerUser, or formAccessCodeForAgents; $err is not valid")
         case _ => JsError("Failure")
       }
   }
