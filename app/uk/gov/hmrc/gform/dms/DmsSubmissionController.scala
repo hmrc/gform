0541aeaed02146f487d709c555adee982f074fc6
seanmurray <sean.murray@digital.hmrc.com>
2019-01-28 11:12:13 +0000

    GFC-705 The envelope expiry date parameter is now formatted.

--- app/uk/gov/hmrc/gform/dms/DmsSubmissionController.scala
+++ app/uk/gov/hmrc/gform/dms/DmsSubmissionController.scala
@@ -46,7 +46,7 @@ class DmsSubmissionController(
       val formTemplateId = FormTemplateId(metadata.dmsFormId)
 
       for {
-        envId <- fileUpload.createEnvelope(formTemplateId)
+        envId <- fileUpload.createEnvelope(formTemplateId)._1
         pdf   <- pdfGenerator.generatePDF(decodedHtml)
         pdfDoc = documentLoader(pdf)
         pdfSummary = PdfSummary(pdfDoc.getNumberOfPages.toLong, pdf)
