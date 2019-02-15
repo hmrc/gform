2bd2d962907c55cc64ed74a1f5a0267b78be9955
louisrobertcamerongoldfinch <louis.goldfinch@digital.hmrc.gov.uk>
2019-02-15 11:49:19 +0000

    GFC755: Disable hyphens in field IDs

--- test/uk/gov/hmrc/gform/sharedmodel/formtemplate/FormComponentSpec.scala
+++ test/uk/gov/hmrc/gform/sharedmodel/formtemplate/FormComponentSpec.scala
@@ -25,4 +25,26 @@ class FormComponentSpec extends Spec {
       FormComponent.format.reads(FormComponent.format.writes(value)) should beJsSuccess(value)
     }
   }
+
+  "formComponentId" should "return a JsError when validated if it contains a hyphen" in {
+    val invalidStr = "foo-bar"
+    verifyError(invalidStr)
+  }
+
+  it should "return a JsSuccess when given an id which does not start with a number and doesn't contain any special " +
+    "characters, apart from underscores." in {
+    forAll(FormComponentGen.formComponentIdGen) { input =>
+      formIdValidator(input.value) should beJsSuccess(input)
+    }
+  }
+
+  it should "return a JsError when validated if it begins with a number" in {
+    forAll(FormComponentGen.idBeginningWithNumberGen) { input =>
+      verifyError(input.value)
+    }
+  }
+
+  private def formIdValidator(s: String) = FormComponentId.validate(s)
+  private def verifyError(input: String) = formIdValidator(input) should beJsError(FormComponentId.errorMessage(input))
+
 }
