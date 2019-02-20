0598ece854d29dfcea440423739df5375f47b024
Lance Walton <461999+lancewalton@users.noreply.github.com>
2019-02-20 23:01:19 +0000

    GFC-799: Allow the handlebars {{#each}} function to work on repeating fields

--- test/uk/gov/hmrc/gform/submission/handlebars/HandlebarsTemplateProcessorModelSpec.scala
+++ test/uk/gov/hmrc/gform/submission/handlebars/HandlebarsTemplateProcessorModelSpec.scala
@@ -16,37 +16,148 @@
 
 package uk.gov.hmrc.gform.submission.handlebars
 
-import java.util
-
-import com.fasterxml.jackson.databind.JsonNode
 import uk.gov.hmrc.gform.Spec
-import uk.gov.hmrc.gform.sharedmodel.form.FormData
-import uk.gov.hmrc.gform.sharedmodel.formtemplate.generators.FormGen
-import com.fasterxml.jackson.databind.node.JsonNodeFactory.{ instance => jsonNodeFactory }
+import uk.gov.hmrc.gform.sharedmodel.form._
+import uk.gov.hmrc.gform.sharedmodel.formtemplate._
 
 class HandlebarsTemplateProcessorModelSpec extends Spec {
-  "apply(Form)" must "create the correct JSON when there are not form fields" in {
-    forAll(FormGen.formGen.map(_.copy(formData = FormData(Seq.empty)))) { form =>
-      HandlebarsTemplateProcessorModel(form).model shouldBe jsonNodeFactory.objectNode()
-    }
-  }
+  "apply(Form, FormTemplate)" must "create the correct JSON" in {
+    val formTemplate =
+      createFormTemplate(
+        createNonRepeatingSection(
+          createNonGroupField("nonRepeatingSection1Field"),
+          createGroupField(
+            "group",
+            createNonGroupField("groupField1"),
+            createNonGroupField("groupField2")
+          )
+        ),
+        createRepeatingSection(
+          createNonGroupField("field1InRepeatingSection"),
+          createNonGroupField("field2InRepeatingSection")
+        )
+      )
 
-  it must "create the correct JSON with multiple form fields" in {
-    forAll(FormGen.formGen, FormGen.formFieldGen, FormGen.formFieldGen) { (form, f1, f2) =>
-      val formWithFields = form.copy(formData = FormData(Seq(f1, f2)))
+    val form = createForm(
+      "nonRepeatingSection1Field"  -> "nonRepeatingSection1FieldValue",
+      "groupField1"                -> "groupField1Value1",
+      "1_groupField1"              -> "groupField1Value2",
+      "groupField2"                -> "groupField2Value1",
+      "1_field1InRepeatingSection" -> "field1InRepeatingSectionValue1",
+      "2_field1InRepeatingSection" -> "field1InRepeatingSectionValue2",
+      "1_field2InRepeatingSection" -> "field2InRepeatingSectionValue1",
+      "2_field2InRepeatingSection" -> "field2InRepeatingSectionValue2"
+    )
 
-      val expectedFieldsMap = new util.HashMap[String, JsonNode]()
-      expectedFieldsMap.put(f1.id.value, jsonNodeFactory.textNode(f1.value))
-      expectedFieldsMap.put(f2.id.value, jsonNodeFactory.textNode(f2.value))
-
-      HandlebarsTemplateProcessorModel(formWithFields).model shouldBe jsonNodeFactory
-        .objectNode()
-        .setAll(expectedFieldsMap)
-    }
+    HandlebarsTemplateProcessorModel(form, formTemplate) shouldBe
+      HandlebarsTemplateProcessorModel(
+        """|{
+           |  "nonRepeatingSection1Field" : "nonRepeatingSection1FieldValue",
+           |  "groupField1" : [
+           |    "groupField1Value1",
+           |    "groupField1Value2"
+           |  ],
+           |  "groupField2" : [
+           |    "groupField2Value1"
+           |  ],
+           |  "field1InRepeatingSection" : [
+           |    "field1InRepeatingSectionValue1",
+           |    "field1InRepeatingSectionValue2"
+           |  ],
+           |  "field2InRepeatingSection" : [
+           |    "field2InRepeatingSectionValue1",
+           |    "field2InRepeatingSectionValue2"
+           |  ]
+           |}""".stripMargin
+      )
   }
 
   "+" must "shallow merge the two models" in {
     HandlebarsTemplateProcessorModel("""{ "a": 1 }""") + HandlebarsTemplateProcessorModel("""{ "b": 2 }""") shouldBe
       HandlebarsTemplateProcessorModel("""{ "a": 1, "b": 2 }""")
   }
+
+  def createForm(fields: (String, String)*): Form =
+    Form(
+      null,
+      null,
+      null,
+      FormTemplateId(""),
+      FormData(fields.map { case (k, v) => FormField(FormComponentId(k), v) }),
+      null,
+      VisitIndex(Set(1)),
+      None)
+
+  def createFormTemplate(sections: Section*): FormTemplate =
+    FormTemplate(
+      FormTemplateId(""),
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      sections.toList,
+      null,
+      null,
+      null
+    )
+
+  def createNonRepeatingSection(fields: FormComponent*): Section =
+    Section(
+      null,
+      null,
+      null,
+      null,
+      null,
+      None,
+      None,
+      null,
+      fields.toList,
+      null,
+      null
+    )
+
+  def createRepeatingSection(fields: FormComponent*): Section =
+    Section(
+      null,
+      null,
+      null,
+      null,
+      null,
+      Some(TextExpression(Value)),
+      Some(TextExpression(Value)),
+      null,
+      fields.toList,
+      null,
+      null
+    )
+
+  def createNonGroupField(id: String): FormComponent =
+    createFormComponent(id, Text(AnyText, Value))
+
+  def createGroupField(id: String, fields: FormComponent*): FormComponent =
+    createFormComponent(id, Group(fields.toList, null))
+
+  def createFormComponent(id: String, componentType: ComponentType): FormComponent =
+    FormComponent(
+      FormComponentId(id),
+      componentType,
+      "",
+      null,
+      null,
+      null,
+      true,
+      true,
+      true,
+      true,
+      true,
+      null
+    )
 }
