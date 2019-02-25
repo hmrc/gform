228f9b0946b5285ec91bdd40e4b36ff3bcb61ace
davidrymer <david.rymer@digital.hmrc.gov.uk>
2019-02-25 09:40:23 +0000

    Moved back to using futures as monadless solution didn't work. Test still not working for ConnectorSpec.

---
+++ test/uk/gov/hmrc/gform/form/ConnectorSpec.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.gform.form
+import org.scalatest.{ MustMatchers, WordSpec }
+import uk.gov.hmrc.gform.auditing.AuditConnector
+import uk.gov.hmrc.play.audit.http.connector.AuditResult
+import uk.gov.hmrc.play.audit.model.DataEvent
+
+import scala.concurrent.Future
+
+class ConnectorSpec extends WordSpec with MustMatchers {
+
+
+  "Handle a request" in {
+
+    val stubbedConn = new AuditConnector {
+      override val connector: DataEvent => Future[AuditResult] = _ => Future.successful(AuditResult.Success)
+      override def logger(msg: String): Unit = {
+        println("msg: "+msg)
+        1 mustBe 2
+
+      }
+    }
+    val body = """"test1": "test1""""
+    val event = DataEvent("src", "type", detail = Map("formId" -> body))
+
+    stubbedConn.sendRequest(event)
+  }
+}
