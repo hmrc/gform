e60aed6a92d540540cc79767ae9723e0123d79f2
Kyle Chown <45938554+kylemchownhmrc@users.noreply.github.com>
2019-02-28 17:08:03 +0000

    Removed unneeded imports

--- app/uk/gov/hmrc/gform/obligation/ObligationController.scala
+++ app/uk/gov/hmrc/gform/obligation/ObligationController.scala
@@ -16,15 +16,11 @@
 
 package uk.gov.hmrc.gform.obligation
 
-import java.text.SimpleDateFormat
-
-import cats.data.NonEmptyList
 import play.api.Logger
 import play.api.mvc.Action
 import uk.gov.hmrc.gform.auditing.loggingHelpers
 import uk.gov.hmrc.gform.controllers.BaseController
-import uk.gov.hmrc.gform.des._
-import uk.gov.hmrc.gform.sharedmodel.{ HmrcTaxPeriodWithEvaluatedId, TaxPeriodIdentifier, TaxResponse }
+import uk.gov.hmrc.gform.sharedmodel.{ HmrcTaxPeriodWithEvaluatedId, TaxResponse }
 
 import scala.concurrent.Future
 
