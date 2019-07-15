37ccbc1006fb0983e209d68627b2bcc74387900f
Lance Walton <461999+lancewalton@users.noreply.github.com>
2019-07-15 15:51:23 +0100

    Logging

--- app/uk/gov/hmrc/gform/submission/handlebars/HandlebarsHttpApiModule.scala
+++ app/uk/gov/hmrc/gform/submission/handlebars/HandlebarsHttpApiModule.scala
@@ -16,12 +16,16 @@
 
 package uk.gov.hmrc.gform.submission.handlebars
 
+import cats.syntax.eq._
+import cats.instances.string._
 import play.api.Logger
 import uk.gov.hmrc.gform.config.ConfigModule
 import uk.gov.hmrc.gform.core.{ FOpt, fOptMonadError }
 import uk.gov.hmrc.gform.sharedmodel.formtemplate.destinations.ProfileName
 import uk.gov.hmrc.gform.wshttp._
 import uk.gov.hmrc.gform.wshttp.HttpClient.HttpClientBuildingSyntax
+import uk.gov.hmrc.http.HeaderNames
+import uk.gov.hmrc.http.logging.Authorization
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -37,9 +41,19 @@ class HandlebarsHttpApiModule(wSHttpModule: WSHttpModule, configModule: ConfigMo
         rootHttpClient
           .buildUri(uri => appendUriSegment(profileConfiguration.baseUrl, uri))
           .buildHeaderCarrier { hc =>
-            Logger.error("Current Headers: " + hc.extraHeaders)
+            Logger.error("Current extraHeader: " + hc.extraHeaders)
+            Logger.error("Current otherHeaders: " + hc.otherHeaders)
             Logger.error("Headers in profile: " + profileConfiguration.httpHeaders)
-            hc.copy(extraHeaders = hc.extraHeaders ++ profileConfiguration.httpHeaders.toSeq)
+            val result = hc.copy(
+              authorization = profileConfiguration.httpHeaders
+                .get(HeaderNames.authorisation)
+                .map(Authorization(_)) orElse hc.authorization,
+              extraHeaders = hc.extraHeaders ++ profileConfiguration.httpHeaders
+                .filterKeys(_ =!= HeaderNames.authorisation)
+                .toSeq
+            )
+            Logger.error("Result of adding to HeaderCarrier: " + result.headers)
+            result
           }
       }
 
