72030271e0b8d678c3d437cbe3c4125832291a3e
Lance Walton <461999+lancewalton@users.noreply.github.com>
2019-09-11 17:42:02 +0100

    MOR-109: Added some logging to help diagnose a problem building the form tree

--- app/uk/gov/hmrc/gform/submission/destinations/FormTreeService.scala
+++ app/uk/gov/hmrc/gform/submission/destinations/FormTreeService.scala
@@ -18,9 +18,11 @@ package uk.gov.hmrc.gform.submission.destinations
 
 import cats.Monad
 import cats.instances.list._
+import cats.syntax.applicative._
 import cats.syntax.flatMap._
 import cats.syntax.functor._
 import cats.syntax.traverse._
+import play.api.Logger
 import uk.gov.hmrc.gform.form.BundledFormTreeNode
 import uk.gov.hmrc.gform.sharedmodel.SubmissionRef
 import uk.gov.hmrc.gform.sharedmodel.form.FormId
@@ -29,16 +31,27 @@ import uk.gov.hmrc.http.HeaderCarrier
 
 class FormTreeService[M[_]: Monad](auditAlgebra: DestinationAuditAlgebra[M]) extends FormTreeAlgebra[M] {
   def getFormTree(rootFormId: FormId)(implicit hc: HeaderCarrier): M[Tree[BundledFormTreeNode]] =
-    auditAlgebra.getLatestForForm(rootFormId).flatMap(buildHierarchyForAudit)
+    for {
+      _    <- Logger.info(s"getFormTree(${rootFormId.value})").pure[M]
+      tree <- auditAlgebra.getLatestForForm(rootFormId).flatMap(buildHierarchyForAudit)
+    } yield tree
 
   private def buildDescendantTreesForChildAudits(childAudits: List[DestinationAudit]) =
     childAudits
       .traverse(buildHierarchyForAudit)
 
   private def buildDescendantTreesForSubmissionRef(submissionRef: SubmissionRef) =
-    auditAlgebra.findLatestChildAudits(submissionRef).flatMap(buildDescendantTreesForChildAudits)
+    for {
+      _           <- Logger.info(s"buildDescendantTreesForSubmissionRef(${submissionRef.value})").pure[M]
+      childAudits <- auditAlgebra.findLatestChildAudits(submissionRef)
+      _ = Logger.info(
+        s"buildDescendantTreesForSubmissionRef(${submissionRef.value}) - child submission references: [${childAudits.map(_.submissionRef.value).mkString(", ")}]")
+      descendantTrees <- buildDescendantTreesForChildAudits(childAudits)
+    } yield descendantTrees
 
   private def buildHierarchyForAudit(audit: DestinationAudit): M[Tree[BundledFormTreeNode]] =
-    buildDescendantTreesForSubmissionRef(audit.submissionRef)
-      .map(Tree(BundledFormTreeNode(audit.formId, audit.submissionRef, audit.formTemplateId), _))
+    for {
+      _     <- Logger.info(s"buildHierarchyForAudit(${audit.submissionRef.value})").pure[M]
+      trees <- buildDescendantTreesForSubmissionRef(audit.submissionRef)
+    } yield Tree(BundledFormTreeNode(audit.formId, audit.submissionRef, audit.formTemplateId), trees)
 }
