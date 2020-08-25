041e7594ea60013d972f4d51a77da91edaf766bd
Girish Kamath <girishkamat@users.noreply.github.com>
2020-08-25 17:21:03 +0100

    GFORMS-873 Support for submission-consolidator destination in form template (#526)
    GFORMS-873 Support for submission-consolidator destination in form template

--- test/uk/gov/hmrc/gform/sharedmodel/formtemplate/destinations/package.scala
+++ test/uk/gov/hmrc/gform/sharedmodel/formtemplate/destinations/package.scala
@@ -57,6 +57,18 @@ package object destinations {
           |  "businessArea": "$businessArea"
           |}""".stripMargin
 
+    case submissionConsolidator: Destination.SubmissionConsolidator =>
+      s"""|{
+          |  "id": "${submissionConsolidator.id.id}",
+          |  ${optionalField("includeIf", Option(submissionConsolidator.includeIf), "true")}
+          |  ${optionalField("failOnError", Option(submissionConsolidator.failOnError), true)}
+          |  "${Destination.typeDiscriminatorFieldName}": "${Destination.submissionConsolidator}",
+          |  "projectId": "${submissionConsolidator.projectId.id}",
+          |  "submissionRef": ${TextExpression.format.writes(submissionConsolidator.submissionRef)},
+          |  "templateId": ${TextExpression.format.writes(submissionConsolidator.templateId)},
+          |  "customerId": ${TextExpression.format.writes(submissionConsolidator.customerId)}
+          |}""".stripMargin
+
     case handlebars: Destination.HandlebarsHttpApi =>
       import handlebars._
       s"""|{
