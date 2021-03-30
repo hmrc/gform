14fe13e0b8dff8786a1ffd9d1c02196a471bac2a
Girish Kamath <girishkamat@users.noreply.github.com>
2021-03-30 09:10:48 +0100

    GFORMS-1177 Implement referral url check (#612)
    * GFORMS-1177 Implement referral url check

--- app/uk/gov/hmrc/gform/sharedmodel/formtemplate/FormTemplate.scala
+++ app/uk/gov/hmrc/gform/sharedmodel/formtemplate/FormTemplate.scala
@@ -39,7 +39,8 @@ case class FormTemplate(
   languages: AvailableLanguages,
   save4LaterInfoText: Option[Save4LaterInfoText],
   summarySection: SummarySection,
-  displayHMRCLogo: Boolean
+  displayHMRCLogo: Boolean,
+  referrerConfig: Option[ReferrerConfig] = None
 ) {
   lazy val expandedFormComponentsInMainSections: List[FormComponent] = sections.flatMap(_.expandedFormComponents)
 }
