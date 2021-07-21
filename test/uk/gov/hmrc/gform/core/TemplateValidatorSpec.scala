30e2b65a1b0fbb92b604e77ad0656de275e7d9d9
Girish Kamath <girishkamat@users.noreply.github.com>
2021-07-21 14:38:06 +0100

    GFORMS-1359 Support document title in add to list page (#648)

--- test/uk/gov/hmrc/gform/core/TemplateValidatorSpec.scala
+++ test/uk/gov/hmrc/gform/core/TemplateValidatorSpec.scala
@@ -699,6 +699,7 @@ class TemplateValidatorSpec extends Spec {
   private def mkAddToList(name: String, pages: NonEmptyList[Page], addAnotherQuestion: FormComponent) =
     Section.AddToList(
       toSmartString(name),
+      Some(toSmartString(name)),
       toSmartString(name),
       toSmartString(name),
       toSmartString(name),
