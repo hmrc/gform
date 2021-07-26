80f43376cefb20b576bd27a8722ea0ab1aa6a72c
girishkamat <girish.kamat@gmail.com>
2021-07-26 18:17:44 +0100

    GFORMS-1140 Fix issue with uploading of db lookup ids in bulk

--- test/uk/gov/hmrc/gform/repo/RepoSpec.scala
+++ test/uk/gov/hmrc/gform/repo/RepoSpec.scala
@@ -193,10 +193,10 @@ class RepoSpec extends FlatSpec with Matchers with DefaultPlayMongoRepositorySup
     val entities = (1 to 10).map(i => buildEntity(i, s"r$i", List(s"p$i")))
     val result1 = repository.upsertBulk(entities).value.futureValue
     result1 shouldBe Right(())
-    val newEntities = (11 to 20).map(i => buildEntity(i, s"r$i", List(s"p$i")))
+    val newEntities = (5 to 11).map(i => buildEntity(i, s"r$i", List(s"p$i")))
     val result2 = repository.upsertBulk(newEntities).value.futureValue
     result2 shouldBe Right(())
-    repository.findAll().futureValue shouldBe entities ++ newEntities
+    repository.findAll().futureValue shouldBe entities ++ List(buildEntity(11, s"r11", List(s"p11")))
   }
 
   "delete" should "remove the entity with given id" in new TestFixture {
