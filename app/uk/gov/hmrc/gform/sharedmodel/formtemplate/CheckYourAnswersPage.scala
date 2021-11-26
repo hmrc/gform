464def6b12c72f7d910b957dd50864cddfd376ad
Dursun Gundogan <78355528+DursunGundogan@users.noreply.github.com>
2021-11-26 14:03:11 +0000

    GFORMS-1553 - Add configuration properties to mini CYA page for Add to List (#709)

--- app/uk/gov/hmrc/gform/sharedmodel/formtemplate/CheckYourAnswersPage.scala
+++ app/uk/gov/hmrc/gform/sharedmodel/formtemplate/CheckYourAnswersPage.scala
@@ -20,7 +20,7 @@ import play.api.libs.json.{ Format, Json }
 import uk.gov.hmrc.gform.sharedmodel.SmartString
 
 case class CheckYourAnswersPage(
-  title: SmartString,
+  title: Option[SmartString],
   updateTitle: SmartString,
   noPIITitle: Option[SmartString],
   noPIIUpdateTitle: Option[SmartString],
