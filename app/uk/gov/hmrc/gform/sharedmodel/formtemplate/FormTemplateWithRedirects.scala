00d387ec96cf206a35214122071bd04d2279b013
Dursun Gundogan <78355528+DursunGundogan@users.noreply.github.com>
2022-05-16 16:32:28 +0100

    GFORMS-1748 - Live Issue : Redirection legacyFormIds functionality is…

--- app/uk/gov/hmrc/gform/sharedmodel/formtemplate/FormTemplateWithRedirects.scala
+++ app/uk/gov/hmrc/gform/sharedmodel/formtemplate/FormTemplateWithRedirects.scala
@@ -21,7 +21,7 @@ import play.api.libs.json.OFormat
 
 final case class FormTemplateWithRedirects(
   formTemplate: FormTemplate,
-  redirects: Option[FormTemplateId] // FormTemplateId which has formTemplate._id in its legacyIds
+  redirect: Option[FormTemplateId] // FormTemplateId which has formTemplate._id in its legacyIds
 )
 
 object FormTemplateWithRedirects {
