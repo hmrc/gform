f37cf531713252fea19da6efdf84ddfc7e6c63b4
Dursun Gundogan <78355528+DursunGundogan@users.noreply.github.com>
2023-02-24 16:35:30 +0000

    GFORMS-2075 - Add a feature to allow a signature box to be added to bottom of acknowledgement PDF (#901)

--- app/uk/gov/hmrc/gform/sharedmodel/formtemplate/PdfCtx.scala
+++ app/uk/gov/hmrc/gform/sharedmodel/formtemplate/PdfCtx.scala
@@ -22,7 +22,8 @@ import uk.gov.hmrc.gform.sharedmodel.SmartString
 case class PdfCtx(
   header: Option[SmartString],
   footer: Option[SmartString],
-  tabularFormat: Option[Boolean]
+  tabularFormat: Option[Boolean],
+  includeSignatureBox: Option[Boolean]
 )
 
 object PdfCtx {
