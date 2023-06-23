ddd6bcd08d04369889c62318b049dd3407b5cde2
Dursun Gundogan <78355528+DursunGundogan@users.noreply.github.com>
2023-06-23 13:39:32 +0100

    GFORMS-2228 - Make the link.download expression dynamic so that new templates can easily be added (#972)

--- app/uk/gov/hmrc/gform/formtemplate/Verifier.scala
+++ app/uk/gov/hmrc/gform/formtemplate/Verifier.scala
@@ -100,7 +100,6 @@ trait Verifier {
       _ <- fromOptA(DestinationsValidator.validateDestinationIncludeIfs(formTemplate.destinations).toEither)
       _ <- fromOptA(FormTemplateValidator.validateTaskListDisplayWidth(formTemplate).toEither)
       _ <- fromOptA(FormTemplateValidator.validateDataThreshold(pages).toEither)
-      _ <- fromOptA(FormTemplateValidator.validateDownloadInternalLink(allExpressions).toEither)
       _ <- fromOptA(FormTemplateValidator.validateFileUpload(formTemplate, appConfig).toEither)
       _ <- fromOptA(FormTemplateValidator.validateChoicesRevealedField(formTemplate).toEither)
       _ <- fromOptA(FormTemplateValidator.validateChoiceFormCtxOptionValues(pages, formTemplate).toEither)
