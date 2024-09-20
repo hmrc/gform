aa14526ded8ce50cc496373b4306967972b4ac0e
Josh Martin <josh.martin@digital.hmrc.gov.uk>
2024-09-20 12:29:29 +0100

    GFORMS-2861 - Update new regex

--- app/uk/gov/hmrc/gform/core/parsers/ValueParser.scala
+++ app/uk/gov/hmrc/gform/core/parsers/ValueParser.scala
@@ -397,7 +397,7 @@ trait ValueParser extends RegexParsers with PackratParsers with BasicParsers {
     str
   }
 
-  lazy val anything: Parser[String] = """.*""".r ^^ { str => str }
+  lazy val notEmpty: Parser[String] = """.+""".r ^^ { str => str }
 
   lazy val alphaNumericWithSpace: Parser[String] = """[a-zA-Z\d ]*""".r ^^ { str =>
     str
@@ -620,7 +620,7 @@ trait ValueParser extends RegexParsers with PackratParsers with BasicParsers {
       OptionDataValue.FormCtxBased(formCtx)
     } | alphabeticOnly ~ expr ^^ { case prefix ~ expr =>
       OptionDataValue.ExprBased(prefix, expr)
-    } | anything ^^ (str => OptionDataValue.StringBased(str))
+    } | notEmpty ^^ (str => OptionDataValue.StringBased(str))
 }
 
 object ValueParser extends ValueParser with PackratParsingHelper {
