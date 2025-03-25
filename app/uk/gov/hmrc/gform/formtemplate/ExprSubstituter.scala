2667615993fd900ac1d4958f81b6cdc246b62a0a
Josef Vlach <38655672+josef-vlach@users.noreply.github.com>
2025-03-25 09:14:44 +0000

    GFORMS-3274 - Fix issue in computation of expressionOutput
    {
      "_id": "expressions-output-with-boolean-expr-reference",
      "version": 1,
      "formName": "Expressions output with booleanExpressions reference",
      "description": "",
      "authConfig": {
        "authModule": "anonymous"
      },
      "expressions": {
        "indieFilmExpenditureTotal": {
          "value": "if indieFilmQualifying then (indieFilmExpenditure - 1000) else 0",
          "type": "sterling"
        }
      },
      "booleanExpressions": {
        "indieFilmQualifying": "indieFilmExpenditure > 1000"
      },
      "expressionsOutput": [
        "indieFilmExpenditureTotal"
      ],
      "sections": [
        {
          "type": "addToList",
          "title": "Expenditures you have added",
          "summaryName": "Expenditures",
          "shortName": "Expenditure $n",
          "description": "**$n. expenditure",
          "summaryDescription": "Expenditures ${indieFilmExpenditureTotal}",
          "addAnotherQuestion": {
            "id": "expenditure",
            "type": "choice",
            "label": "Do you want to add another expenditure?",
            "format": "yesno"
          },
          "pages": [
            {
              "title": "Indie film expenditure.",
              "fields": [
                {
                  "id": "indieFilmExpenditure",
                  "label": "What is the total expenditure to date?",
                  "helpText": "Enter in GBP.",
                  "type": "text",
                  "format": "positiveSterling"
                }
              ]
            }
          ]
        }
      ],
      "declarationSection": {
        "shortName": "Declaration",
        "title": "Declaration",
        "fields": []
      },
      "acknowledgementSection": {
        "title": "Acknowledgement Page",
        "fields": []
      },
      "destinations": [
        {
          "id": "eeittReturnApi",
          "type": "handlebarsHttpApi",
          "failOnError": false,
          "profile": "des",
          "convertSingleQuotes": true,
          "uri": "cross-regime/return/APD/eeits/{{APDReg}}",
          "method": "POST",
          "payload": " { 'total': '{{indieFilmExpenditureTotal}}' }}"
        }
      ]
    }

--- app/uk/gov/hmrc/gform/formtemplate/ExprSubstituter.scala
+++ app/uk/gov/hmrc/gform/formtemplate/ExprSubstituter.scala
@@ -142,9 +142,6 @@ object ExprSubstituter extends Substituter[ExprSubstitutions, FormTemplate] {
 
   def substitute(substitutions: ExprSubstitutions, t: FormTemplate): FormTemplate =
     implicitly[Substituter[ExprSubstitutions, FormTemplate]].substitute(substitutions, t)
-
-  def substituteExpr(substitutions: ExprSubstitutions, t: Expr): Expr =
-    implicitly[Substituter[ExprSubstitutions, Expr]].substitute(substitutions, t)
 }
 
 trait SubstituteExpressions {
